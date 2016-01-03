0f7e7a315d53243570f058dae473f541e1693fe9
saraogiraj94 <saraogi.raj40@gmail.com>
2016-01-03 10:13:17 +0530

    SuceessVolley

---
+++ app/src/main/java/com/example/raj/volleytest/Config.java
@@ -0,0 +1,16 @@
+package com.example.raj.volleytest;
+
+/**
+ * Created by raj on 2/1/16.
+ */
+public class Config {
+    public static final String DATA_URL = "http://192.168.1.2/st/stationary.php";
+
+    //JSON TAGS
+    public static final String TAG_IMAGE_URL = "pimage";
+    public static final String TAG_NAME = "pname";
+    public static final String TAG_DESC = "pdesc";
+    public static final String TAG_PRICE = "price";
+
+
+}
